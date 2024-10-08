72e2d884c21220f654ae0c920081239f582f553e
Pavol Padysak <pavol.padysak@student.tuke.sk>
2024-10-08 13:19:53 +0200

    Add user add new property. Upload except image.

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -74,13 +74,13 @@ const PropertyAddForm = () => {
       amenities: updatedAmenities,
     }));
   };
-  const handleImageChange = () => {
+  const handleImageChange = (e) => {
     const { files } = e.target;
     // Clone images array
     const updatedImages = [...fields.images];
     // Add new files to the array
     for (const file of files) {
-      updatedImages.push(files[i]);
+      updatedImages.push(file);
     }
     // Update the state with the new array
     setFields((prevFields) => ({
@@ -91,7 +91,11 @@ const PropertyAddForm = () => {
 
   return (
     mounted && (
-      <form>
+      <form
+        action="/api/properties"
+        method="POST"
+        encType="multipart/form-data"
+      >
         <h2 className="text-3xl text-center font-semibold mb-6">
           Add Property
         </h2>
@@ -497,7 +501,7 @@ const PropertyAddForm = () => {
           <input
             type="text"
             id="seller_name"
-            name="seller_info.name."
+            name="seller_info.name"
             className="border rounded w-full py-2 px-3"
             placeholder="Name"
             value={fields.seller_info.name}
@@ -555,6 +559,7 @@ const PropertyAddForm = () => {
             accept="image/*"
             multiple
             onChange={handleImageChange}
+            required
           />
         </div>
 
